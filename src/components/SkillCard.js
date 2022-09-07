d50a54bdd0e6e192eb25e79aae54282ed925153c
sergio <dirtycrew666@gmail.com>
2022-09-07 11:33:06 +0300

    Create a card to display a skill from skills

---
+++ src/components/SkillCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SkillCard = ({ text, setSkillEntry, skillEntry, card }) => {
+
+    const handleRemove = () => {
+        setSkillEntry(skillEntry.filter(el => el.id !== card.id))
+    }
+
+    return (
+        <li 
+        className="px-4 py-2 border-solid border-2 rounded-lg border-indigo-300 text-indigo-600 flex flex-col items-center justify-center"
+        >
+            {text}
+            <svg onClick={handleRemove} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4 hover:text-red-400 cursor-pointer">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </li>
+    )
+}
+
+export default SkillCard
