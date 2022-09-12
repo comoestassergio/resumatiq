182cadc34204d5a1b45a0935b3c1157a7d571e54
sergio <dirtycrew666@gmail.com>
2022-09-12 17:08:25 +0300

    Add styles and logic to Title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Title = () => {
+const Title = ({ userData, setUserData }) => {
+
+    const [input, setInput] = useState('')
+
+    const handleInput = (e) => {
+        setInput(e.target.value)
+    }
+
+    const handleFinish = () => {
+        setUserData({
+            ...userData,
+            title: input,
+        })
+    }
+
     return (
-        <></>
+        <div className="flex flex-col items-center justify-center gap-5 min-h-screen bg-base-200">
+            <form className="flex gap-2">
+                <input
+                    onChange={handleInput} 
+                    type="text" 
+                    placeholder="Position you're going for" 
+                    className="input input-bordered w-80 max-w-xs" 
+                    name="title"
+                    value={input}
+                    maxLength={45}
+                    required
+                />
+            </form>
+            {input.length > 0 &&
+                <button onClick={handleFinish} className="btn btn-primary">Finish</button>
+            }
+        </div>
     )
 }
 
