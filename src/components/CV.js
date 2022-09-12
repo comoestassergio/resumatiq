ca74dc1a5897b1d1ae58d419829f2bf3ab4c418a
sergio <dirtycrew666@gmail.com>
2022-09-12 15:10:51 +0300

    Add Experience, skills and Education to CV comp

--- src/components/CV.js
+++ src/components/CV.js
@@ -5,9 +5,9 @@ const CV = () => {
         <div className="min-h-screen bg-base-100 py-5 px-7 lg:max-w-5xl lg:mx-auto">
             <div className="mb-4 relative after:absolute after:w-full after:bg-slate-400 after:h-0.5">
                 <h1 className="text-5xl font-bold">John Doe</h1>
-                <h3 className="text-2xl uppercase text-slate-600">Title</h3>
+                <h3 className="text-lg uppercase text-slate-600">Title</h3>
             </div>
-            <div className="flex flex-col gap-1.5">
+            <div className="flex flex-row gap-3 flex-wrap">
                 <p className="flex flex-row gap-2 items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -31,7 +31,7 @@ const CV = () => {
                 </p>
                 <p className="flex gap-2 items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.3">
+                        <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" stroke-width="1.3">
                             <path d="M18 8L5 12.5L9.5 14M18 8l-8.5 6M18 8l-4 10.5L9.5 14"/>
                             <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2S2 6.477 2 12s4.477 10 10 10Z"/>
                         </g>
@@ -40,13 +40,13 @@ const CV = () => {
                 </p>
                 <p className="flex gap-2 items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                        <path fill="currentColor" fill-rule="evenodd" d="M1 2.838A1.838 1.838 0 0 1 2.838 1H21.16A1.837 1.837 0 0 1 23 2.838V21.16A1.838 1.838 0 0 1 21.161 23H2.838A1.838 1.838 0 0 1 1 21.161V2.838Zm8.708 6.55h2.979v1.496c.43-.86 1.53-1.634 3.183-1.634c3.169 0 3.92 1.713 3.92 4.856v5.822h-3.207v-5.106c0-1.79-.43-2.8-1.522-2.8c-1.515 0-2.145 1.089-2.145 2.8v5.106H9.708V9.388Zm-5.5 10.403h3.208V9.25H4.208v10.54ZM7.875 5.812a2.063 2.063 0 1 1-4.125 0a2.063 2.063 0 0 1 4.125 0Z" clip-rule="evenodd"/>
+                        <path fill="currentColor" fillRule="evenodd" d="M1 2.838A1.838 1.838 0 0 1 2.838 1H21.16A1.837 1.837 0 0 1 23 2.838V21.16A1.838 1.838 0 0 1 21.161 23H2.838A1.838 1.838 0 0 1 1 21.161V2.838Zm8.708 6.55h2.979v1.496c.43-.86 1.53-1.634 3.183-1.634c3.169 0 3.92 1.713 3.92 4.856v5.822h-3.207v-5.106c0-1.79-.43-2.8-1.522-2.8c-1.515 0-2.145 1.089-2.145 2.8v5.106H9.708V9.388Zm-5.5 10.403h3.208V9.25H4.208v10.54ZM7.875 5.812a2.063 2.063 0 1 1-4.125 0a2.063 2.063 0 0 1 4.125 0Z" clip-rule="evenodd"/>
                     </svg>
                     LinkedIn
                 </p>
                 <p className="flex gap-2 items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                        <path fill="currentColor" fill-rule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12Z" clip-rule="evenodd"/>
+                        <path fill="currentColor" fillRule="evenodd" d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385c.6.105.825-.255.825-.57c0-.285-.015-1.23-.015-2.235c-3.015.555-3.795-.735-4.035-1.41c-.135-.345-.72-1.41-1.23-1.695c-.42-.225-1.02-.78-.015-.795c.945-.015 1.62.87 1.845 1.23c1.08 1.815 2.805 1.305 3.495.99c.105-.78.42-1.305.765-1.605c-2.67-.3-5.46-1.335-5.46-5.925c0-1.305.465-2.385 1.23-3.225c-.12-.3-.54-1.53.12-3.18c0 0 1.005-.315 3.3 1.23c.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23c.66 1.65.24 2.88.12 3.18c.765.84 1.23 1.905 1.23 3.225c0 4.605-2.805 5.625-5.475 5.925c.435.375.81 1.095.81 2.22c0 1.605-.015 2.895-.015 3.3c0 .315.225.69.825.57A12.02 12.02 0 0 0 24 12c0-6.63-5.37-12-12-12Z" clip-rule="evenodd"/>
                     </svg>
                     GitHub
                 </p>
@@ -56,6 +56,77 @@ const CV = () => {
                     </svg>
                     Twitter
                 </p>
+            </div>
+            <div className="grid grid-cols-2 mt-8 gap-x-10 gap-y-2">
+                <section className="flex flex-col gap-2 row-span-2">
+                    <h2 className="text-2xl font-bold uppercase mb-4">Experience</h2>
+                    <div className="flex flex-col gap-1 border-2 border-solid border-gray-300 rounded-lg p-5">
+                        <p className="text-xl uppercase text-gray-600">Position</p>
+                        <p className="text-lg font-bold">Workplace</p>
+                        <p className="flex gap-2 text-sm items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                            </svg>
+                            2020 - 2022
+                        </p>
+                        <ul className="list-disc ml-5 mt-2">
+                            <li>
+                                First task
+                            </li>
+                            <li>
+                                Second task
+                            </li>
+                            <li>
+                                Third task
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="flex flex-col gap-1 border-2 border-solid border-gray-300 rounded-lg p-5">
+                        <p className="text-xl uppercase text-gray-600">Position</p>
+                        <p className="text-lg font-bold">Workplace</p>
+                        <p className="flex gap-2 text-sm items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                            </svg>
+                            2020 - 2022
+                        </p>
+                        <ul className="list-disc ml-5 mt-2">
+                            <li>
+                                First task
+                            </li>
+                            <li>
+                                Second task
+                            </li>
+                            <li>
+                                Third task
+                            </li>
+                        </ul>
+                    </div>
+                </section>
+                <section>
+                    <h2 className="text-2xl font-bold uppercase mb-4">Skills</h2>
+                    <ul className="flex flex-row gap-3 pt-5 flex-wrap">
+                        <li className="border-2 border-gray-300 capitalize px-5 py-2 rounded-lg">Skill</li>
+                        <li className="border-2 border-gray-300 capitalize px-5 py-2 rounded-lg">Skill</li>
+                        <li className="border-2 border-gray-300 capitalize px-5 py-2 rounded-lg">Skill</li>
+                        <li className="border-2 border-gray-300 capitalize px-5 py-2 rounded-lg">Skill</li>
+                        <li className="border-2 border-gray-300 capitalize px-5 py-2 rounded-lg">Skill</li>
+                        <li className="border-2 border-gray-300 capitalize px-5 py-2 rounded-lg">Skill</li>
+                    </ul>
+                </section>
+                <section>
+                    <h2 className="text-2xl font-bold uppercase mb-4">Education</h2>
+                    <div className="flex flex-col gap-1 border-2 border-gray-300 rounded-lg p-5">
+                        <p className="text-xl uppercase text-gray-600">University</p>
+                        <p className="text-lg capitalize font-bold">Degree</p>
+                        <p className="flex gap-2 text-sm items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                            </svg>
+                            2020 - 2022
+                        </p>
+                    </div>
+                </section>
             </div>
         </div>
     )
