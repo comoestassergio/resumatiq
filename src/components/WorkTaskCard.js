72766daea6a60617d85a5e079c55ad26a43b466f
sergio <dirtycrew666@gmail.com>
2022-09-13 10:53:52 +0300

    Create WorkTaskCard component to be rendered into WorkTasks

---
+++ src/components/WorkTaskCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const WorkTaskCard = ({ text, setTaskEntry, taskEntry, card }) => {
+
+    const handleRemove = () => {
+        setTaskEntry(taskEntry.filter(el => el.id !== card.id))
+    }
+
+    return (
+        <li 
+        className="px-4 py-2 border-solid border-2 rounded-lg w-96 border-gray-300 text-gray-600 bg-base-200 flex items-center justify-between gap-1 capitalize"
+        >
+            {text}
+            <svg onClick={handleRemove} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-400 hover:text-red-600 cursor-pointer">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </li>
+    )
+}
+
+export default WorkTaskCard
