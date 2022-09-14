02ae08c4c66439f3f2aa4de0d818f7f9a403c0ab
sergio <dirtycrew666@gmail.com>
2022-09-14 10:10:13 +0300

    Change width for smaller screens

--- src/components/WorkTaskCard.js
+++ src/components/WorkTaskCard.js
@@ -8,7 +8,7 @@ const WorkTaskCard = ({ text, setTaskEntry, taskEntry, card }) => {
 
     return (
         <li 
-        className="px-4 py-2 border-solid border-2 rounded-lg w-96 border-gray-300 text-gray-600 bg-base-200 flex items-center justify-between gap-1 capitalize"
+        className="px-4 py-2 border-solid border-2 rounded-lg w-80 md:w-96 border-gray-300 text-gray-600 bg-base-200 flex items-center justify-between gap-1 capitalize"
         >
             {text}
             <svg onClick={handleRemove} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-400 hover:text-red-600 cursor-pointer">
