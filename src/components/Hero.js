6cc7ed86151366067b98dbdb81c1a28f4bd245da
sergio <dirtycrew666@gmail.com>
2022-09-15 15:03:45 +0300

    Add getting from local storage and button to open existing cv

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,24 @@
 import React from "react"
 
-const Hero = ({ setStart }) => {
+const Hero = ({ setStart, isLocalCV, setTitleDone, titleDone, setUserData }) => {
 
-    const handleClick = () => {
+    const handleNew = () => {
         setStart(true)
     }
+
+    const handleLocal = () => {
+        getFromLocalStorage()
+        setTitleDone(!titleDone)
+    }
+
+    const getFromLocalStorage = () => {
+        if (localStorage.getItem('CV') === null){
+          localStorage.setItem('CV', JSON.stringify({}))
+        } else {
+          let localCV = JSON.parse(localStorage.getItem('CV'))
+          setUserData(localCV)
+        }
+      }
 
     return (
         <div className="hero min-h-screen bg-base-200">
@@ -12,7 +26,15 @@ const Hero = ({ setStart }) => {
                 <div className="max-w-md">
                 <h1 className="text-5xl font-bold">Résumé Creator</h1>
                 <p className="py-6">Creating résumés made as easy as never before</p>
-                <button onClick={handleClick} className="btn btn-primary">Get Started</button>
+                {!isLocalCV && 
+                    <button onClick={handleNew} className="btn btn-primary">Get Started</button>
+                }
+                {isLocalCV &&
+                    <div className="flex flex-col gap-2">
+                        <button onClick={handleLocal} className="btn btn-secondary">Open last created</button>
+                        <button onClick={handleNew} className="btn btn-ghost">Create new CV</button>
+                    </div>
+                }
                 </div>
             </div>
         </div>
