bed871765facb1fd18d3c802d57f74a23553b5e5
sergio <dirtycrew666@gmail.com>
2022-09-16 09:29:07 +0300

    Change name to Resumatic

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,8 +24,8 @@ const Hero = ({ setStart, isLocalCV, setTitleDone, titleDone, setUserData }) => 
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content text-center">
                 <div className="max-w-md">
-                <h1 className="text-5xl font-bold">Résumé Creator</h1>
-                <p className="py-6">Creating résumés made as easy as never before</p>
+                <h1 className="text-5xl font-bold">Résumatic</h1>
+                <p className="py-6 italic">Creating résumés made as easy as never before</p>
                 {!isLocalCV && 
                     <button onClick={handleNew} className="btn btn-primary">Get Started</button>
                 }
