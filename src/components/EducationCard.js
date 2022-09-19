e1a3c266babfed4ee132b4c5baec2e6ec95fbbbc
sergio <dirtycrew666@gmail.com>
2022-09-19 12:05:05 +0300

    Change styling for small screens

--- src/components/EducationCard.js
+++ src/components/EducationCard.js
@@ -7,7 +7,7 @@ const EducationCard = ({ university, degree, startYear, gradYear, education, set
     }
 
     return (
-        <li className="flex flex-row items-center justify-between w-96 bg-slate-50 rounded-lg p-5 card">
+        <li className="flex flex-col md:flex-row text-center md:text-left gap-2 md:gap-0 items-center justify-between w-80 md:w-96 bg-slate-50 rounded-lg p-5 card">
             <div>
                 <h3 className="text-lg uppercase font-bold">
                     {university}
@@ -19,7 +19,7 @@ const EducationCard = ({ university, degree, startYear, gradYear, education, set
                     {startYear} - {gradYear}
                 </p>
             </div>
-            <button onClick={handleClick} className="btn btn-circle btn-outline">
+            <button onClick={handleClick} className="btn btn-circle btn-outline mt-3 md:mt-0">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
             </button>
         </li>
