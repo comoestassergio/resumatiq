c7fa12533d094ad4cdfe2bfee54c3386d0c580eb
sergio <dirtycrew666@gmail.com>
2022-09-21 20:00:38 +0300

    Add titles and form color change on error

--- src/components/EducationForm.js
+++ src/components/EducationForm.js
@@ -21,6 +21,8 @@ const EducationForm = ( { education, setEducation, setIsClicked, isClicked } ) =
                 id: uniqid(),
             }
         )
+
+        checkEmptyInput(name, value, e)
     }
 
     const handleSubmit = (e) => {
@@ -52,6 +54,23 @@ const EducationForm = ( { education, setEducation, setIsClicked, isClicked } ) =
         setIsClicked(!isClicked)
     }
 
+    const checkEmptyInput = (name, value, e) => {
+        switch (name) {
+            case 'university':
+                value.length < 1 ? e.target.classList.add('input-error') : e.target.classList.remove('input-error')
+                break
+            case 'degree':
+                value.length < 1 ? e.target.classList.add('input-error') : e.target.classList.remove('input-error')
+                break
+            case 'startYear':
+                value.length !== 4 ? e.target.classList.add('input-error') : e.target.classList.remove('input-error')
+                break
+            default: 
+                console.log('default')
+                break
+        }
+    } 
+
     return (
         <form onSubmit={handleSubmit} className="flex flex-col items-center gap-3 w-80">
             <h2 className="text-lg mb-3 uppercase font-semibold">New Entry</h2>
@@ -61,7 +80,8 @@ const EducationForm = ( { education, setEducation, setIsClicked, isClicked } ) =
                     placeholder="University"
                     name="university" 
                     className='input input-bordered w-full max-w-md col-span-2' 
-                    value={educationEntry.university} 
+                    value={educationEntry.university}
+                    title={'Cannot be empty'} 
                     required
             />
             <input 
@@ -71,6 +91,7 @@ const EducationForm = ( { education, setEducation, setIsClicked, isClicked } ) =
                     name="degree" 
                     className='input input-bordered w-full max-w-md col-span-2' 
                     value={educationEntry.degree}  
+                    title={'Cannot be empty'} 
                     required
             />
             <input 
@@ -82,6 +103,7 @@ const EducationForm = ( { education, setEducation, setIsClicked, isClicked } ) =
                     value={educationEntry.startYear}
                     minLength={4}
                     maxLength={4}  
+                    title={'Four dgigits long'}
                     required
             />
             <input 
@@ -93,6 +115,7 @@ const EducationForm = ( { education, setEducation, setIsClicked, isClicked } ) =
                     value={educationEntry.graduationYear}  
                     minLength={4}
                     maxLength={4}
+                    title={'Four dgigits long'}
                     required
             />
             <button type="submit" className="btn w-80">
