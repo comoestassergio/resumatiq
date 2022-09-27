e5b3ac095ab9392ead77a7c02dac441b7c488a31
sergio <dirtycrew666@gmail.com>
2022-09-27 13:18:55 +0300

    Change work task color for dark mode

--- src/components/WorkTaskCard.js
+++ src/components/WorkTaskCard.js
@@ -10,7 +10,7 @@ const WorkTaskCard = ({ text, setTaskEntry, taskEntry, card }) => {
         <li 
         className="px-4 py-2 border-solid border-2 rounded-lg w-80 md:w-96 border-gray-300 text-gray-600 bg-base-200 flex items-center justify-between gap-1"
         >
-            <div className=" max-w-xs first-letter:capitalize">
+            <div className=" max-w-xs first-letter:capitalize dark:text-accent">
                 {text}
             </div>
             <svg onClick={handleRemove} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-gray-400 hover:text-red-600 cursor-pointer">
